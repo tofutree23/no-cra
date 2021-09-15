bbe0a4103c968a0a5a64b720b2229cd29892113a
Jonghyung <tofutree23@gmail.com>
2021-09-15 06:51:11 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.listen("3000", () => {
+  console.log("server is listening on 3000");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello, world");
+});
